d97b380af3ffb42f96c09863296a38f259aa7e6b
Thibaut Géry <thibaut.gery@gmail.com>
2016-05-15 14:04:43 +0200

    add swagger documentation

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,23 +1,9 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.junit.*;
 
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
+import org.junit.Test;
 import play.twirl.api.Content;
 
-import static play.test.Helpers.*;
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 /**
@@ -31,9 +17,14 @@ public class ApplicationTest {
     @Test
     public void simpleCheck() {
         int a = 1 + 1;
-        assertEquals(2, a);
+        assertThat(a).isEqualTo(2);
     }
 
-
+    @Test
+    public void renderTemplate() {
+        Content html = views.html.documentation.render("Swagger Documentation");
+        assertThat(html.contentType()).isEqualTo("text/html");
+        assertThat(html.body()).contains("swagger");
+    }
 
 }
