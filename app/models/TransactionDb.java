6d69c2fa169b4d95324377e020800e8ac9755261
Thibaut Géry <thibaut.gery@gmail.com>
2016-05-16 11:49:59 +0200

    lock the collection when working on it

--- app/models/TransactionDb.java
+++ app/models/TransactionDb.java
@@ -17,36 +17,43 @@ final public class TransactionDb {
     }
 
     public void putTransaction(Transaction transaction) {
-        transactions.put(transaction.getId(), transaction);
-
+        synchronized (this) {
+            transactions.put(transaction.getId(), transaction);
+        }
     }
 
     public Optional<Transaction> getTransaction(Double id) {
-        return ofNullable(transactions.get(id));
+        synchronized (this) {
+            return ofNullable(transactions.get(id));
+        }
     }
 
     public List<Double> getTransaction(String type) {
-        return transactions.values().stream()
-                .filter(t1 -> t1.getType().equals(type))
-                .map(Transaction::getId)
-                .collect(Collectors.toList());
+        synchronized (this) {
+            return transactions.values().stream()
+                    .filter(t1 -> t1.getType().equals(type))
+                    .map(Transaction::getId)
+                    .collect(Collectors.toList());
+        }
     }
 
     public List<Transaction> getLinkedTransaction(Double id) {
         List<Transaction> result = new ArrayList<>();
         Optional<Double> idToCheck = ofNullable(id);
-        while(idToCheck.isPresent()) {
-            Optional<Transaction> currentOpt = getTransaction(idToCheck.get());
-            idToCheck = empty();
-            if(currentOpt.isPresent()) {
-                Transaction current = currentOpt.get();
-                result.add(current);
-                if(!isParentInArray(result, current)) {
-                    idToCheck = ofNullable(current.getParent_id());
+        synchronized (this) {
+            while (idToCheck.isPresent()) {
+                Optional<Transaction> currentOpt = getTransaction(idToCheck.get());
+                idToCheck = empty();
+                if (currentOpt.isPresent()) {
+                    Transaction current = currentOpt.get();
+                    result.add(current);
+                    if (!isParentInArray(result, current)) {
+                        idToCheck = ofNullable(current.getParent_id());
+                    }
                 }
             }
+            return result;
         }
-        return result;
     }
 
     private boolean isParentInArray(List<Transaction> transactions, Transaction current) {
